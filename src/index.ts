08cedae1d65cbc1f674432e04f6377abe196b926
Brandon Wie <brandonwie.cs@gmail.com>
2023-02-01 12:36:23 +0900

    test01

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+export default function useAdBlockDetector() {
+  const [isAdBlockDetected, setIsAdBlockDetected] = useState<boolean>(false);
+
+  useEffect(() => {
+    const adBlockDetector = document.createElement('div');
+    adBlockDetector.id = 'ads-by-google';
+    adBlockDetector.className = 'test-ad';
+    document.body.appendChild(adBlockDetector);
+
+    if (getComputedStyle(adBlockDetector).display === 'none') {
+      setIsAdBlockDetected(true);
+      alert('AdBlock is enabled on this page.');
+    }
+    return () => {
+      document.body.removeChild(adBlockDetector);
+    };
+  }, []);
+
+  return isAdBlockDetected;
+}
